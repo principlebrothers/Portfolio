4de2f63b2f9d71dd1ab11d749f8e0a6591715f02
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-27 22:54:50 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import './App.css'
+import Topbar from './components/topbar/Topbar'
+import Intro from './components/intro/Intro'
+import Portfolio from './components/portfolio/Portfolio'
+import Works from './components/works/Works'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import './App.scss'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Counter</h1>
+      <Topbar />
+      <section className='sections'>
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </section>
     </div>
   )
 }
