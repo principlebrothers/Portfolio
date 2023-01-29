f46e904e15be4aab8ef385ff03674c808d5b4e51
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-29 05:50:14 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Topbar from './components/topbar/Topbar'
-import Intro from './components/intro/Intro'
-import Portfolio from './components/portfolio/Portfolio'
-import Works from './components/works/Works'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import './App.scss'
+import { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import Works from './components/works/Works';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Menu from './components/menu/Menu'
+import './App.scss';
 
 function App() {
-
+  const [expandMenu, setExpandMenu] = useState(false);
   return (
-    <div className="App">
-      <Topbar />
+    <div className='App'>
+      <Navbar expandMenu={expandMenu} setExpandMenu={setExpandMenu} />
+      <Menu expandMenu={expandMenu} setExpandMenu={setExpandMenu} />
       <section className='sections'>
         <Intro />
         <Portfolio />
@@ -19,7 +22,7 @@ function App() {
         <Contact />
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
