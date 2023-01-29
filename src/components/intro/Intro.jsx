79e2a5bf35dda4d2e4a3f7f816cd5af36136218d
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-29 20:27:51 +0000

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,41 @@
 import './Intro.scss'
+import Ernest_profile2 from '../../assets/Ernest_profile2.png';
+import down from '../../assets/down.png';
+import Typewriter from 'typewriter-effect';
 
 function Intro() {
+
   return (
-    <div className='intro' id='intro'>Intro</div>
-  )
+    <article className='intro' id='intro'>
+      <div className='left'>
+        <div className='img__container'>
+          <img src={Ernest_profile2} alt='profile picture' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hello, my name is</h2>
+          <h1>Ernest A. Adonu</h1>
+          <h3>
+            Full-stack Developer,{' '}
+            <span>
+              <Typewriter
+                options={{
+                  strings: ['a Writer', 'and a Typist'],
+                  autoStart: true,
+                  delay: 75,
+                  loop: true,
+                }}
+              />
+            </span>
+          </h3>
+        </div>
+        <a href='#portfolio'>
+          <img src={down} alt='down' />
+        </a>
+      </div>
+    </article>
+  );
 }
 
 export default Intro
