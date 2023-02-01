546593697dcf7c3037c590d619012e2264c9b0d9
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-01 22:27:17 +0000

    Create technologies.jsx

---
+++ src/components/skills/technologies.jsx
@@ -0,0 +1,20 @@
+export const frontendSkills = [
+  'HTML5',
+  'CSS3',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Bootstrap',
+  'Material UI',
+  'Sass',
+];
+
+export const backendSkills = ['Ruby on Rails', 'PostgreSQL', 'Ruby', 'MySQL'];
+
+
+export const contentWritingSkills = [
+  'SEO',
+  'Content Writing',
+  'Blogging',
+  'Copywriting',
+];
