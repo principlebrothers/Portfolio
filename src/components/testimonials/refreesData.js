6306b0a00bd69d92c25e1be236a12063eb13e2dc
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-02 14:38:05 +0000

    Create refreesData.js

---
+++ src/components/testimonials/refreesData.js
@@ -0,0 +1,38 @@
+import linkedin from '../../assets/linkedin.jpg';
+
+export const testimonials = [
+  {
+    id: 1,
+    name: 'Tracey Kadenyi',
+    title: 'Full-stack Web Developer',
+    img: 'https://media.licdn.com/dms/image/D4D03AQGN0VL089d6pw/profile-displayphoto-shrink_100_100/0/1671032146137?e=1680739200&v=beta&t=94NiUAMx29lXLaPCX5i3IXCT770Yg4yDJm3VjKM50Mg',
+    icon: linkedin,
+    desc: 'Ernest is a competent developer. Not only is he knowledgeable, particularly in front-end Web development (reactjs) but also he is a determined learner and can grasp concepts quickly and easily. I have worked with him and would do so again.',
+  },
+  // {
+  //   id: 2,
+  //   name: 'Seth Bizimana',
+  //   title: 'Full-stack Web Developer',
+  //   img: 'https://media.licdn.com/dms/image/D4E03AQE_3uaTsalSHA/profile-displayphoto-shrink_100_100/0/1671548065118?e=1680739200&v=beta&t=ROcwd7b5HFWlAcgLlbbg9T0jgvjw2hpntpeypHamjQ8',
+  //   icon: linkedin,
+  //   desc: "Ernest is an amazing person he is particularly talented at making people around him feel comfortable, technically he is always eager to learn and he is practically good at it, considering how he grasped new concepts quickly. another thing is that while working with Ernest I always feel confident to produce a professional and good-looking UI and let me tell you something. He never fails me even once. by the time our project was done, I felt like we'd known each other for years. I can't recommend him enough",
+  //   featured: true,
+  // },
+  {
+    id: 2,
+    name: 'Ushindi Bienvenu',
+    title: 'Full-stack Web Developer',
+    img: 'https://media.licdn.com/dms/image/C4D03AQEjodjuxyUrlg/profile-displayphoto-shrink_100_100/0/1661894186402?e=1680739200&v=beta&t=TtM4ZW9uSDE-7VJNobRMz2EW5SzU2lGmrosgR0UdGM8',
+    icon: linkedin,
+    desc: 'Ernest is a smiling and positive guy, willing to go all the way to get the job done and does his best every day. I was impressed by his speed of learning, writing skills and his punctuality. Technically, he knows how to find different resources, talk to the right people and find a solution no matter what. It’s without hesitation that I recommend Him if you are looking for a good Full-Stack Developer',
+    featured: true,
+  },
+  {
+    id: 4,
+    name: 'Priscilla Oyebade',
+    title: 'Full-stack Developer',
+    img: 'https://media.licdn.com/dms/image/D4D03AQGT6zEK9wq1MA/profile-displayphoto-shrink_100_100/0/1671477441923?e=1680739200&v=beta&t=lFLprECJ2jOWXf0yynLHUoHM6aTW-lTNj5k94MU7y14',
+    icon: linkedin,
+    desc: 'I highly recommend Ernest, he is patient and ensures he get positive output in whatever he is doing. I had worked with him a couple of times and can boldly say that it is great working with him.',
+  },
+];
