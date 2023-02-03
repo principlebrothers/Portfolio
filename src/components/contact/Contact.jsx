92632efa5f125552a7f2232ada5ffce5d185aaa4
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-03 01:57:59 +0000

    Update contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,84 @@
 import './Contact.scss'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import shake from '../../assets/shake.svg'
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_fosk3ae',
+        'template_jfq71nz',
+        form.current,
+        'kAlIkH1sedkgQJBkj'
+      )
+      .then(
+        (result) => {
+          toast.success(`🦄 Message sent successfully`, {
+            position: 'top-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+          });
+        },
+        (error) => {
+          toast.error('Message not sent', {
+            position: 'top-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+          });
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <div className='contact' id='contact'>Contact</div>
-  )
+    <article className='contact' id='contact'>
+      <div className='left'>
+        <img src={shake} alt='hand shake' />
+      </div>
+      <div className='right'>
+        <h1>Contact</h1>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+        >
+          <input
+            type='name'
+            placeholder='Enter your name'
+            name='from_name'
+            required
+          />
+          <input
+            type='email'
+            placeholder='yourmail@domain.com'
+            name='from_email'
+            required
+          />
+          <textarea placeholder='Message' name='message' required />
+          <button type='submit'>
+            <span>SEND</span>
+          </button>
+        </form>
+        <ToastContainer />
+      </div>
+    </article>
+  );
 }
 
 export default Contact
