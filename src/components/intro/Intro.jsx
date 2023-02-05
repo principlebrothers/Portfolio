ce2401eaf9e67aed160d99bcca010fca75411ca8
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-05 18:53:53 +0000

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,7 @@ import './Intro.scss'
 import Ernest_profile2 from '../../assets/Ernest_profile2.png';
 import down from '../../assets/down.png';
 import Typewriter from 'typewriter-effect';
+import Resume from '../../assets/Resume.pdf';
 
 function Intro() {
 
@@ -14,21 +15,37 @@ function Intro() {
       </div>
       <div className='right'>
         <div className='wrapper'>
-          <h2>Hello, my name is</h2>
-          <h1>Ernest A. Adonu</h1>
-          <h3>
-            Full-stack Developer,{' '}
+          <h2>
+            Hello! I'm Ernest,{' '}
             <span>
               <Typewriter
                 options={{
-                  strings: ['a Content Writer', 'and a Typist'],
+                  strings: [
+                    'Full-stack Developer',
+                    'Content Writer',
+                    'Freelance Typist',
+                  ],
                   autoStart: true,
                   delay: 75,
                   loop: true,
                 }}
               />
             </span>
-          </h3>
+          </h2>
+          <p>
+            Motivated software engineer with more than 1300 hours of teamwork
+            experience. Skilled at producing top-notch web apps with a heavy
+            empahsis on effectiveness and user experiences. Process automation
+            is a topic I enjoy. You can download my{' '}
+            <a
+              href={Resume}
+              download='Ernest Anyewe Adonu Resume'
+              target='_blank'
+            >
+              résumé
+            </a>
+            to learn more about my experiences.
+          </p>
         </div>
         <a href='#projects'>
           <img src={down} alt='down' />
