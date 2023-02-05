37d2c5cd7bcd889f50c67b24fcf65ab5a04c0184
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-05 20:41:26 +0000

    Update projectsData.jsx

--- src/components/projects/projectsData.jsx
+++ src/components/projects/projectsData.jsx
@@ -8,32 +8,25 @@ export const featuredData = [
   },
   {
     id: 2,
-    title: 'Lavish Cuisine',
-    image: 'https://i.postimg.cc/G2DTGz66/Lavish-Cuisine.png',
-    live: 'https://lavish-cuisine.netlify.app/',
-    github: 'https://github.com/okoyecharles/lavish-cuisine',
+    title: 'Trek Bike Rentals',
+    image: 'https://i.postimg.cc/YSMGm8qH/Trek.png',
+    live: 'https://deploy-preview-31--rentbikes.netlify.app/',
+    github: 'https://github.com/DuaneDave/Bike-rental',
   },
   {
     id: 3,
-    title: 'Lavish Cuisine',
-    image: 'https://i.postimg.cc/G2DTGz66/Lavish-Cuisine.png',
-    live: 'https://lavish-cuisine.netlify.app/',
-    github: 'https://github.com/okoyecharles/lavish-cuisine',
+    title: "Space Traveler's Hub",
+    image: 'https://i.postimg.cc/J4Rb9bkt/Space-travel.png',
+    live: 'https://priscillaernestspacetravel.netlify.app/',
+    github: 'https://github.com/OyePriscilla/spacetravellershub',
   },
   {
     id: 4,
-    title: 'Lavish Cuisine',
-    image: 'https://i.postimg.cc/G2DTGz66/Lavish-Cuisine.png',
-    live: 'https://lavish-cuisine.netlify.app/',
-    github: 'https://github.com/okoyecharles/lavish-cuisine',
-  },
-  {
-    id: 5,
-    title: 'Lavish Cuisine',
-    image: 'https://i.postimg.cc/G2DTGz66/Lavish-Cuisine.png',
-    live: 'https://lavish-cuisine.netlify.app/',
-    github: 'https://github.com/okoyecharles/lavish-cuisine',
-  },
+    title: 'Awesome Books',
+    image: 'https://i.postimg.cc/tR80JB6Y/awesome-books.png',
+    live: 'https://principlebrothers.github.io/Awesome-books-modified/',
+    github: 'https://github.com/principlebrothers/Awesome-books-modified',
+  }
 ];
 
 export const mobileData = [
